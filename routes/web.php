14833598db528f816d1051ac32e48534b9364dcd
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-12-01 15:50:22 +0000

    Basic Crud

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http\Middleware\First;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\ProductController;
 
 Route::view('/first', 'first')->name('first');
 
 Route::get('/loop-test/{num}/{name}', [TestController::class, 'loop'])->name('loop.test');
+
+Route::resource('product', ProductController::class, ['names' => 'product']);
 
